3c99424f802793c8a25af08c5e0b8eb47529ec05
Igor_Demchenko <idemch777@gmail.com>
2022-10-01 08:28:35 +0300

    Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число : ");
+string userInputa = Console.ReadLine() ?? "";
+int a = int.Parse(userInputa);
+Console.Write("Введите второе число : ");
+string userInputb = Console.ReadLine() ?? "";
+int b = int.Parse(userInputb);
+Console.Write("Введите третье число : ");
+string userInputc = Console.ReadLine() ?? "";
+int c = int.Parse(userInputc);
+int max = a;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("max -> " + max);
