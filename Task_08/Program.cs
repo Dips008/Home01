86c578c38a59f56612bb5cbfda647a3623fff5fb
Igor_Demchenko <idemch777@gmail.com>
2022-10-01 09:27:22 +0300

    Task_08

---
+++ Task_08/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число : ");
+string userInput = Console.ReadLine() ?? "";
+int n = int.Parse(userInput);
+int count = 1;
+int k = n;
+while (count < n)
+{
+    if (k % 2 == 0)
+    {
+        Console.Write(k + ", ");
+    }
+    k--;
+    count++;
+}
