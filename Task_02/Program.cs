8f3915734653cad37b8b65d9f8d9b31f3f4c10c0
Igor_Demchenko <idemch777@gmail.com>
2022-10-01 08:05:42 +0300

    Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число а: ");
+string userInputa = Console.ReadLine() ?? "";
+int a = int.Parse(userInputa);
+Console.Write("Введите число b: ");
+string userInputb = Console.ReadLine() ?? "";
+int b = int.Parse(userInputb);
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+}
