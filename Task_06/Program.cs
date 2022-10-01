7928d549ccc8fbc2ac1334e3b5fb465488ab2dd5
Igor_Demchenko <idemch777@gmail.com>
2022-10-01 08:41:07 +0300

    Task_06

---
+++ Task_06/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число : ");
+string userInput = Console.ReadLine() ?? "";
+int a = int.Parse(userInput);
+Console.Write("Число " + a);
+if (a % 2 == 0)
+{
+    Console.Write(" -> четное");
+}
+else
+{
+    Console.Write(" -> не четное");
+}
