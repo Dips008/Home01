4edd3d7b863db00370ec2e8a448471498218eee5
Igor_Demchenko <idemch777@gmail.com>
2022-10-02 20:55:32 +0300

    Add Task Dog and Friends, Proba

---
+++ Dog_friend/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int distance = 10000;
+int friend = 2;
+int time = 0;
+while (distance > 10)
+{
+    if (friend == 2)
+    {
+        time = distance / (DogSpeed + SecondFriendSpeed);
+        distance = distance - time * (FirstFriendSpeed + SecondFriendSpeed);
+        count++;
+        friend = 1;
+    }
+    if (friend == 1)
+    {
+        time = distance / (DogSpeed + FirstFriendSpeed);
+        distance = distance - time * (FirstFriendSpeed + SecondFriendSpeed);
+        count++;
+        friend = 2;
+    }
+}
+Console.WriteLine("Собака пробежала : " + count + " раз");
---
+++ Proba/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число : ");
+string userInput = Console.ReadLine() ?? "";
+int n = int.Parse(userInput);
+int k = n;
+while (n > 1)
+{
+    if (n % 2 == 0)
+    {
+        Console.Write(n + ", ");
+    }
+    n--;
+}
